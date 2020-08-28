4afe552b5b4930bf4c43050cc74c90edafca0c68
djtoohey <djtoohey@gmail.com>
2020-08-28 12:01:56 +0930

    routing fix

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -32,37 +32,4 @@ module.exports = function (app) {
         });
       });
   })
-
-  // app.get("/best", function (req, res) {
-  //   db.Couple.findAll(
-  //     {
-  //       where: {
-  //         compatability: {
-  //           [Op.or]: {
-  //             [Op.gt]: 50,
-  //             [Op.eq]: null
-  //           },
-  //         },
-  //       },
-  //     })
-  //     .then(function (data) {
-
-  //       var bestCouples = [];
-
-  //       data.forEach(couple => {
-  //         bestCouples.push(couple);
-  //       });
-
-
-
-  //       res.render("best", {
-  //         couple: bestCouples
-  //       });
-  //     });
-  // })
-
-  // app.get("/worst", function (req, res) {
-  //   res.redirect("/api/worst");
-  // })
-
 }
